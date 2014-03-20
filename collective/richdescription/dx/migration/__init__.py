2e0e9aeee93b7a6cfa0d0f66d2000bc3d909c1d9
Johannes Raggam <raggam-nl@adm.at>
2014-03-20 17:14:10 +0100

    dont patch the ATCTBaseMigrator but register an adapter via the new ICustomMigrator interface, to add the richdescriptionmigrator to all Archetypes content types

--- collective/richdescription/dx/migration/__init__.py
+++ collective/richdescription/dx/migration/__init__.py
@@ -1,18 +1,40 @@
 from Products.CMFPlone.utils import safe_unicode
-from plone.app.textfield.value import RichTextValue
+from plone.app.contenttypes.migration.migration import ICustomMigrator
 from plone.app.contenttypes.migration.utils import ATCT_LIST
-from plone.app.contenttypes.migration import migration
+from plone.app.textfield.value import RichTextValue
+from zope.component import adapter
+from zope.interface import Interface
+from zope.interface import implementer
+from collective.richdescription.dx.behaviors import IRichDescription
+
+import logging
+logger = logging.getLogger(__name__)
 
 
-def richdescription_migrator(old, new):
-    field = old.getField('richdescription')
-    mime_type = field.getContentType(old)
-    raw_text = safe_unicode(field.getRaw(old))
-    if raw_text.strip() == '':
-        return
-    richtext = RichTextValue(raw=raw_text, mimeType=mime_type,
-                             outputMimeType='text/x-html-safe')
-    new.richdescription = richtext
+@implementer(ICustomMigrator)
+@adapter(Interface)
+class RichDescriptionMigrator(object):
+
+    def __init__(self, context):
+        self.context = context
+
+    def migrate(self, old, new):
+        behavior = IRichDescription(new, None)
+        if behavior:
+            field = old.getField('richdescription')
+            mime_type = field.getContentType(old)
+            raw_text = safe_unicode(field.getRaw(old))
+            if raw_text.strip() == '':
+                return
+            richtext = RichTextValue(raw=raw_text, mimeType=mime_type,
+                                     outputMimeType='text/x-html-safe')
+            behavior.richdescription = richtext
+
+            logger.info(
+                "Migrating richdescription value: %s" % (
+                    len(raw_text) > 10 and '%s...' % raw_text[:10] or raw_text
+                )
+            )
 
 
 def patch_ATCT_LIST():
@@ -23,13 +45,3 @@ def patch_ATCT_LIST():
             # ... because patch_ATCT_LIST might be called more than once
             ext.append('richdescription')
 patch_ATCT_LIST()
-
-
-orig_ATCTBaseMigrator = migration.ATCTBaseMigrator
-class ATCTBaseMigrator(orig_ATCTBaseMigrator):
-    """Patch ATCTBaseMigrator to do the richdescription.
-    Please note: works until a derived class overwrited the custom method.
-    """
-    def custom(self):
-        richdescription_migrator(self.old, self.new)
-migration.ATCTBaseMigrator = ATCTBaseMigrator
