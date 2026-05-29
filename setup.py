177f06f6bf7b9dbdb020c1cd93bc5eeca1602839
Johannes Raggam <thetetet@gmail.com>
2026-05-29 15:55:26 +0200

    Back to development: 3.0.4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = "3.0.3"
+version = "3.0.4.dev0"
 
 long_description = "{}\n{}".format(
     open("README.rst").read(), open("CHANGES.rst").read()
